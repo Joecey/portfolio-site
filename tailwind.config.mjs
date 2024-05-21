aef28a93787d37fda824b3c6b5fc02ff4b358f77
Joe Linogao <linogaoj@tcd.ie>
2024-05-21 12:16:04 +0100

    successfully added shadcn and a button

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 import defaultTheme from 'tailwindcss/defaultTheme'
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: ['class'],
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    prefix: '',
     theme: {
+        container: {
+            center: true,
+            padding: '2rem',
+            screens: {
+                '2xl': '1400px',
+            },
+        },
         extend: {
             colors: {
                 myTheme: {
@@ -17,11 +26,25 @@ export default {
                 body: ['Space Grotesk', ...defaultTheme.fontFamily.sans],
                 heading: ['Young Serif', ...defaultTheme.fontFamily.serif],
             },
+            keyframes: {
+                'accordion-down': {
+                    from: { height: '0' },
+                    to: { height: 'var(--radix-accordion-content-height)' },
+                },
+                'accordion-up': {
+                    from: { height: 'var(--radix-accordion-content-height)' },
+                    to: { height: '0' },
+                },
+            },
+            animation: {
+                'accordion-down': 'accordion-down 0.2s ease-out',
+                'accordion-up': 'accordion-up 0.2s ease-out',
+            },
         },
         fontWeight: {
             normal: '400',
             bold: '700',
         },
     },
-    plugins: [],
+    plugins: [require('tailwindcss-animate')],
 }
