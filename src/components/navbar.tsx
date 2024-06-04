0d70317fb14ca53679f65d8f98d7f9de541d86dc
Joe Linogao <linogaoj@tcd.ie>
2024-06-04 10:20:28 +0100

    navbar left algined with max-width-7xl

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -25,18 +25,20 @@ export default function GlobalNavbar() {
 
     return (
         <header>
-            <nav id="global-navbar" className="z-10 flex w-full flex-row justify-center gap-8 p-8">
-                {navigationLinks.map((navItem: navigationLinkProps) => {
-                    return (
-                        <a
-                            href={navItem.href}
-                            className="font-heading transition-colors ease-in hover:text-myTheme-accent"
-                        >
-                            {navItem.title}
-                        </a>
-                    )
-                })}
-            </nav>
+            <div className="z-10 flex w-full justify-center">
+                <nav id="global-navbar" className="flex w-full max-w-7xl gap-8 p-8">
+                    {navigationLinks.map((navItem: navigationLinkProps) => {
+                        return (
+                            <a
+                                href={navItem.href}
+                                className="font-heading transition-colors ease-in hover:text-myTheme-accent"
+                            >
+                                {navItem.title}
+                            </a>
+                        )
+                    })}
+                </nav>
+            </div>
         </header>
     )
 }
